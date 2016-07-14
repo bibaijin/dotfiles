72eb59d095d4da71a8b8dd91418938de886fec20
kaizhang <kaizhang.shanxi@gmail.com>
2016-07-14 10:41:47 +0800

    add aliases-yixin.zsh

---
+++ zsh/aliases-yixin.zsh
@@ -0,0 +1,9 @@
+ssh_measure_cluster() {
+  ssh root@10.106.170.$1
+}
+alias ssh_measure=ssh_measure_cluster
+
+ssh_produce_cluster() {
+  ssh kaizhang33@10.120.248.22
+}
+alias ssh_produce=ssh_produce_cluster
